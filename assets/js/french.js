f73a4521898197b4cffe1ae506665df458b55c66
Nurlana Huseyinova <80hd0r0@code.edu.az>
2023-06-14 09:16:09 +0400

    french language students data add,edit

--- assets/js/french.js
+++ assets/js/french.js
@@ -1,11 +1,24 @@
 let FRENCH_URL = `http://localhost:8080/french`;
 let frenchData = document.querySelector("#frenchData");
+let frenchForm = document.querySelector("#frenchForm");
+let frenchFirstName = document.querySelector("#frenchFirstName");
+let frenchLastName = document.querySelector("#frenchLastName");
+let frenchEmail = document.querySelector("#frenchEmail");
+let germanSubject = document.querySelector("#germanSubject");
+let frenchPhoneStart = document.querySelector("#frenchPhoneStart");
+let frenchPhoneEnd = document.querySelector("#frenchPhoneEnd");
+let frenchSubmit = document.querySelector("#frenchSubmit");
+let frenchFormHead = document.querySelector("#frenchFormHead");
+let editStatus5 = false;
+let editId5 = null;
+let copyArr5 = [];
 
 async function getFrenchData() {
-    frenchData.innerHTML = "";
+  frenchData.innerHTML = "";
   let res = await axios(FRENCH_URL);
   let data = await res.data;
-  data.forEach((info) => {
+  copyArr5 = data;
+  copyArr5.forEach((info) => {
     frenchData.innerHTML += `
                 <tr>
                   <td>${info.id}</td>
@@ -30,4 +43,56 @@ getFrenchData();
 async function deleteBtnFrench(id, btn) {
   await axios.delete(`${FRENCH_URL}/${id}`);
   btn.closest("tr").remove();
+}
+
+frenchForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  let obj = {
+    firstName: frenchFirstName.value,
+    lastName: frenchLastName.value,
+    email: frenchEmail.value,
+    subject: germanSubject.value || "French",
+    phone: ` ${frenchPhoneStart.value} ${frenchPhoneEnd.value}`,
+  };
+  if (
+    frenchFirstName.value &&
+    frenchLastName.value &&
+    frenchEmail.value &&
+    germanSubject &&
+    frenchPhoneEnd.value
+  ) {
+    if (editStatus5) {
+      await axios.patch(`${FRENCH_URL}/${editId5}`, obj);
+      getFrenchData();
+      frenchSubmit.innerHTML = "Submit";
+      frenchFormHead.innerHTML = "Add Students";
+      frenchFirstName.innerHTML = "";
+      frenchLastName.innerHTML = "";
+      frenchEmail.innerHTML = "";
+      germanSubject.innerHTML = "";
+      editStatus5 = false;
+    } else {
+      await axios.post(`${FRENCH_URL}`, obj);
+      getFrenchData();
+      console.log(obj);
+    }
+  } else {
+    alert("The form is not completed!");
+  }
+});
+
+async function editBtnFrench(id) {
+  editId5 = id;
+  editStatus5 = true;
+  copyArr5.find((el) => el.id == editId5);
+  let res = await axios(`${FRENCH_URL}/${id}`);
+  let data = await res.data;
+  frenchFirstName.value = data.firstName;
+  frenchLastName.value = data.lastName;
+  frenchEmail.value = data.email;
+  germanSubject.value = data.subject;
+  frenchPhoneEnd.value = data.phone;
+  // console.log(editId5);
+  frenchSubmit.innerHTML = "Edit";
+  frenchFormHead.innerHTML = "Edit Students";
 }
