14ef07425073318d8a194f7477dfb5377f53961e
Nurlana Huseyinova <80hd0r0@code.edu.az>
2023-06-20 11:13:08 +0400

    get comment data to comment section

---
+++ assets/js/coursesComment.js
@@ -0,0 +1,31 @@
+let COMMENTS_URL = `http://localhost:3000/comment`;
+let commentSlide = document.querySelector(".commentSlide");
+let commentCopyArr = [];
+async function getCommentData() {
+    commentSlide.innerHTML = "";
+  let res = await axios(COMMENTS_URL);
+  let data = await res.data;
+  commentCopyArr = data;
+  commentCopyArr.forEach((graduate) => {
+    commentSlide.innerHTML += `
+    <div class="swiper-slide">
+    <div class="item">
+      <div class="commentNote">
+        <p>
+          ${graduate.comment.slice(0,200)}
+        </p>
+      </div>
+      <div class="itemImg">
+        <img src=" ${graduate.photo}" alt="" />
+        <div class="imgNote">
+          <h3> ${graduate.firstName}  ${graduate.lastName}</h3>
+          <p> ${graduate.subject}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+                 
+          `;
+  });
+}
+getCommentData();
